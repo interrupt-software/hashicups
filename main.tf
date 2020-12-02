118cbf91c53eeb61ef704f42b1fd2bb0ed2ad85f
gcastill0 <gilberto@hashicorp.com>
2020-12-02 13:13:37 -0500

    Change happens

--- main.tf
+++ main.tf
@@ -18,3 +18,24 @@ provider "hashicups" {}
 # output "coffee" {
 #   value = module.psl.coffee
 # }
+
+variable "coffee_name" {
+  type    = string
+  default = "Vagrante espresso"
+}
+
+data "hashicups_coffees" "all" {}
+
+# Returns all coffees
+output "all_coffees" {
+  value = data.hashicups_coffees.all.coffees
+}
+
+# Only returns packer spiced latte
+output "coffee" {
+  value = {
+    for coffee in data.hashicups_coffees.all.coffees :
+    coffee.id => coffee
+    if coffee.name == var.coffee_name
+  }
+}
