4881018b3040289cd2ef7656c3c5d440816256fd
gcastill0 <gilberto@hashicorp.com>
2020-12-02 13:12:36 -0500

    Change happens

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# terraform {
-#   required_providers {
-#     hashicups = {
-#       source  = "interrupt-software/seng/hashicups"
-#       version = "0.2.0"
-#     }
-#   }
-# }
-#
+terraform {
+  required_providers {
+    hashicups = {
+      source  = "interrupt-software/seng/hashicups"
+      version = "0.2.0"
+    }
+  }
+}
+
 provider "hashicups" {}
 
-module "psl" {
-  source      = "app.terraform.io/interrupt-software/hashicups/provider"
-  version     = "0.2.0"
-  coffee_name = "Packer Spiced Latte"
-}
-
-output "coffee" {
-  value = module.psl.coffee
-}
+# module "psl" {
+#   source      = "app.terraform.io/interrupt-software/hashicups/provider"
+#   version     = "0.2.0"
+#   coffee_name = "Packer Spiced Latte"
+# }
+#
+# output "coffee" {
+#   value = module.psl.coffee
+# }
